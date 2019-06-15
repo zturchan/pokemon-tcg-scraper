67ed8fca7c0ed0f241648fb644ea3b67d4b42442
Zak Turchansky <zak.turchansky@gmail.com>
2019-06-15 15:19:45 -0600

    Initial module creation

---
+++ scraper.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        Pokemon TCG Scraper
+# Purpose:     Scrapes the POKEMON TCG API for all cards and renders them in
+#              a desired Excel format.
+#
+# Author:      Zak Turchansky
+#
+# Created:     15-06-2019
+# Copyright:   (c) Zak Turchansky 2019
+# Licence:     LICENSE.txt
+#-------------------------------------------------------------------------------
+
+from pokemontcgsdk import Card
+
+def main():
+ pass
+
+if __name__ == '__main__':
+    main()
