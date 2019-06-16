7c3a38b01de4c24c776f549b06635f51ad42bd7a
Zak Turchansky <zak.turchansky@gmail.com>
2019-06-15 18:12:04 -0600

    Fetch default cards from generations

--- scraper.py
+++ scraper.py
@@ -11,8 +11,15 @@
 #-------------------------------------------------------------------------------
 
 from pokemontcgsdk import Card
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from os import remove
+from pathlib import Path
+
 
 def main():
+    print("Downloading all cards... Please be patient")
+
     fields_remain_same = ['id',
                           'name',
                           'national_pokedex_number',
@@ -31,6 +38,37 @@ def main():
                           'converted_retreat_cost',
                           'types',
                           'evolves_from']
+    #cards = Card.all()
+    cards = Card.where(set='generations')
+    create_workbook(cards, fields_remain_same)
+
+def create_workbook(cards, default_fields):
+    if Path("pkmn_output.xlsx").is_file():
+        remove("pkmn_output.xlsx")
+    wb = Workbook()
+    ws = wb.active
+    ws.append(create_header_row(default_fields))
+    for cell in ws["1:1"]:
+        cell.font = Font(bold=True)
+    for card in cards:
+        row = []
+        for field in default_fields:
+            value = getattr(card, field)
+            if isinstance(value, list) and len(value) == 1:
+                row.append(value[0])
+            elif not isinstance(value, list):
+                row.append(value)
+            else:
+                raise Exception("Unexpected data type found: " + field +
+                                ". Value is " + str(value))
+        ws.append(row)
+
+
+    wb.save("pkmn_output.xlsx")
+
+def create_header_row(default_fields):
+    row = default_fields.copy()
+    return row
 
 if __name__ == '__main__':
     main()
